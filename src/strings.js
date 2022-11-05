d5233a97482bdfcb05e746a7be58a6b591a2cecf
Mark P <mj.poso18@gmail.com>
2022-11-05 09:43:31 +0800

    Initialize header section

--- src/strings.js
+++ src/strings.js
@@ -1,7 +1,35 @@
-const encrypt = btoa;
-const decrypt = atob;
+/*  MDN Suggestions for Base64 Encryption
+ *  https://developer.mozilla.org/en-US/docs/Glossary/Base64#solution_1_%E2%80%93_escaping_the_string_before_encoding_it
+ */
 
-const strings = { NAME: 'TWFyayBQb3Nv' };
+function encrypt(str) {
+  return btoa(encodeURIComponent(str));
+}
+
+function decrypt(str) {
+  return decodeURIComponent(atob(str));
+}
+
+const strings = {
+  NAME: 'TWFyayUyMFBvc28=',
+  HEADER_MAIN: ['SGkhJTIwSSVFMiU4MCU5OW0=', 'TWFyay4='],
+  HEADER_TEXT_HEADING: 'V2hvJTIwYW0lMjBJJTNG',
+  HEADER_TEXT: [
+    'SSVFMiU4MCU5OW0lMjBh',
+    'd2ViJTIwYW5kJTIwc29mdHdhcmUlMjBkZXZlbG9wZXI=',
+    'KFNvb24lRTIlODQlQTIp',
+    'd2hvJTIwc3R1ZGllZA==',
+    'Y29tcHV0ZXIlMjBlbmdpbmVlcmluZy4=',
+    'SSUyMGNyYWZ0JTIwZXhwZXJpZW5jZXMlMjB1c2luZyUyMHRvb2xzJTIwaW4=',
+    'SlM=',
+    'JTdD',
+    'VFM=',
+    'JTI2',
+    'UHl0aG9u',
+    'Zm9yJTIwYm90aCUyMG1vYmlsZSUyMGFuZCUyMHRoZSUyMHdlYi4=',
+    'UFMlM0ElMjBJJTIwZGFiYmxlJTIwaW4lMjBkYXRhJTIwc2NpZW5jZSUyMHRvbyE=',
+  ],
+};
 
 export default strings;
-export { decrypt };
+export { encrypt, decrypt };
