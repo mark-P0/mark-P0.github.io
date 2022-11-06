397f3c2297dca1cfb4dd0584fa4776c8ad2b7305
Mark P <mj.poso18@gmail.com>
2022-11-06 16:03:55 +0800

    Add hidden header link

--- src/strings.js
+++ src/strings.js
@@ -10,10 +10,16 @@ function decrypt(str) {
   return decodeURIComponent(atob(str));
 }
 
+// const _ = encrypt('...');
+// console.log([_, decrypt(_)]);
+
+/* cspell:disable */
 const strings = {
   NAME: 'TWFyayUyMFBvc28=',
   HEADER_MAIN: ['SGkhJTIwSSVFMiU4MCU5OW0=', 'TWFyay4='],
   HEADER_TEXT_HEADING: 'V2hvJTIwYW0lMjBJ',
+  HEADER_TEXT_HEADING_LINK:
+    'aHR0cHMlM0ElMkYlMkZlbi53aWtpcGVkaWEub3JnJTJGd2lraSUyRldob2FtaQ==',
   HEADER_TEXT: [
     'SSVFMiU4MCU5OW0lMjBh',
     'd2ViJTIwYW5kJTIwc29mdHdhcmUlMjBkZXZlbG9wZXI=',
@@ -41,6 +47,7 @@ const strings = {
     'aHR0cHMlM0ElMkYlMkZ3d3cubGlua2VkaW4uY29tJTJGaW4lMkZwb3NvLW1hcmtqb2huJTJG',
   FOOTER_ADDRESS: 'bWFpbHRvJTNBbWFya2pvaG4ucG9zbyU0MG91dGxvb2sucGg=',
 };
+/* cspell:enable */
 
 export default strings;
 export { encrypt, decrypt };
