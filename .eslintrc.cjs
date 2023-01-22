734c638bacb0f0dd6f399fb8a6a1ab763a285a0f
Mark P <mj.poso18@gmail.com>
2023-01-22 21:18:20 +0800

    Add ESLint plugin for a11y as recommended by `eslint-plugin-astro`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:astro/recommended',
     'prettier',
   ],
