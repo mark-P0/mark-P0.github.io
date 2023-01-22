8cdf20d62c738f4f32df941560a6919a1930eedc
Mark P <mj.poso18@gmail.com>
2023-01-22 21:07:54 +0800

    Explicitly declare ESLint, Prettier, and CSpell as dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/**
+ * https://github.com/typescript-eslint/typescript-eslint/issues/2153
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
