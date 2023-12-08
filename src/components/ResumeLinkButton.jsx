fb1312d25923716b7a5d82b5bf80bb15a1c393b7
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2023-12-08 08:59:18 +0800

    Set resume link button as Solid component

---
+++ src/components/ResumeLinkButton.jsx
@@ -0,0 +1,11 @@
+export function ResumeLinkButton() {
+  return (
+    <a
+      href="#"
+      target="_blank"
+      class="bg-primary text-white px-4 py-2 rounded-full font-title font-bold text-xl"
+    >
+      Resumé
+    </a>
+  );
+}
