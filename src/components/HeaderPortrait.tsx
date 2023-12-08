f3bb94db8c737a5caff0dabf2430c10c5dec0dc8
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2023-12-08 12:37:38 +0800

    Show loading indicator on header portrait

--- src/components/HeaderPortrait.tsx
+++ src/components/HeaderPortrait.tsx
@@ -1,3 +1,27 @@
+import { BsCircleFill } from "solid-icons/bs";
+import { Show, createEffect, createSignal } from "solid-js";
+
+async function sleep(secs: number) {
+  return new Promise((resolve) => setTimeout(resolve, secs * 1000));
+}
+
 export function HeaderPortrait() {
-  return <div class="w-full aspect-[3/4] bg-primary/50">portrait</div>;
+  const [src, setSrc] = createSignal<string | null>(null);
+  createEffect(() => {
+    (async () => {
+      await sleep(1);
+      setSrc("");
+    })();
+  });
+
+  return (
+    <div class="w-full aspect-[3/4] bg-primary/50 grid place-items-center">
+      <Show
+        when={src() !== null}
+        fallback={<BsCircleFill class="w-12 h-12 animate-ping" />}
+      >
+        <div></div>
+      </Show>
+    </div>
+  );
 }
