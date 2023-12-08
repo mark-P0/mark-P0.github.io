8fc541cb4ec7de2f34072941688dbb1b59fad827
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2023-12-09 07:20:31 +0800

    Retrieve project data from Firebase

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,8 +1,16 @@
-import { Show, createSignal } from "solid-js";
+import { Show, createEffect, createSignal } from "solid-js";
+import { getProjects, type Project } from "../firebase/rtdb.ts";
 import { LoadingIndicator } from "./LoadingIndicator.tsx";
 
 export function ProjectList() {
-  const [projects, setProjects] = createSignal<unknown[] | null>(null);
+  const [projects, setProjects] = createSignal<Project[] | null>(null);
+  createEffect(() => {
+    async function initializeProjects() {
+      const projects = await getProjects();
+      setProjects(projects);
+    }
+    initializeProjects();
+  });
 
   const Fallback = (
     <div class="grid place-items-center">
@@ -11,7 +19,7 @@ export function ProjectList() {
   );
   return (
     <Show when={projects() !== null} fallback={Fallback}>
-      <div>project list</div>
+      <div>{JSON.stringify(projects())}</div>
     </Show>
   );
 }
