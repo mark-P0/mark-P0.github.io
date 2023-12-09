d53470797647104802cfb551daa5aab281164930
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2023-12-10 07:34:25 +0800

    Add site info in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from "astro/config";
-import Tailwind from "@astrojs/tailwind";
 import SolidJS from "@astrojs/solid-js";
+import Tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mark-p0.github.io/",
   integrations: [
     Tailwind({
       applyBaseStyles: false,
