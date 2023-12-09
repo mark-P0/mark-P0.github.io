00fed81e1b66b361b354b8138fe5fef18f7b2b25
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2023-12-09 10:51:45 +0800

    Add tag icons from Simple Icons via `solid-icons`

--- src/components/TagIcons.tsx
+++ src/components/TagIcons.tsx
@@ -1,3 +1,23 @@
 import type { IconTypes } from "solid-icons";
+import {
+  SiBootstrap,
+  SiFirebase,
+  SiJavascript,
+  SiPython,
+  SiReact,
+  SiTailwindcss,
+  SiTypescript,
+  SiVite,
+  SiWebpack,
+} from "solid-icons/si";
 
 export const TagIconMap = new Map<string, IconTypes>();
+TagIconMap.set("TypeScript", SiTypescript);
+TagIconMap.set("React", SiReact);
+TagIconMap.set("Tailwind", SiTailwindcss);
+TagIconMap.set("Vite", SiVite);
+TagIconMap.set("Firebase", SiFirebase);
+TagIconMap.set("JavaScript", SiJavascript);
+TagIconMap.set("Webpack", SiWebpack);
+TagIconMap.set("Bootstrap", SiBootstrap);
+TagIconMap.set("Python", SiPython);
