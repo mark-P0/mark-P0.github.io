a9e774d02cc3ab3c87484e5de1393739f39bb1df
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-10-06 00:59:16 +0800

    refactor(navbar): use common section records

--- src/components/react/Navbar.tsx
+++ src/components/react/Navbar.tsx
@@ -1,51 +1,63 @@
 /** @jsxImportSource react */
 
 import { useEffect } from "react";
+import { SECTION_RECORDS, type SectionRecord } from "../../section-records.ts";
 
 function useSectionObserver() {
   useEffect(() => {
     const root = document.documentElement;
     root.classList.add("group/scrollspy");
 
-    const profile = document.getElementById("who-am-i");
-    const projects = document.getElementById("what-i-do");
-    const contacts = document.getElementById("talk-to-me");
+    const sectionElementRecords = SECTION_RECORDS.map(({ id }) => ({
+      id,
+      element: document.getElementById(id),
+      isVisible: false,
+    }));
 
-    function callback(
-      entries: IntersectionObserverEntry[]
-      // observer: IntersectionObserver
-    ) {
-      let isProfileVisible = false;
-      let isProjectsVisible = false;
-      let isContactsVisible = false;
-      for (const entry of entries) {
-        const { target, isIntersecting } = entry;
-        if (target === profile && isIntersecting) isProfileVisible = true;
-        else if (target === projects && isIntersecting)
-          isProjectsVisible = true;
-        else if (target === contacts && isIntersecting)
-          isContactsVisible = true;
+    const observerCallback: IntersectionObserverCallback = (entries) => {
+      /** On every callback run, assume none of the sections are visible */
+      for (const section of sectionElementRecords) {
+        section.isVisible = false;
       }
 
-      if (isProfileVisible) root.setAttribute("data-section", "who-am-i");
-      else if (isProjectsVisible)
-        root.setAttribute("data-section", "what-i-do");
-      else if (isContactsVisible)
-        root.setAttribute("data-section", "talk-to-me");
-    }
+      for (const entry of entries) {
+        const { target, isIntersecting } = entry;
+        if (!isIntersecting) continue;
+
+        for (const section of sectionElementRecords) {
+          if (target !== section.element) continue;
+
+          section.isVisible = true;
+          break;
+        }
+      }
+
+      for (const section of sectionElementRecords) {
+        if (!section.isVisible) continue;
+
+        root.setAttribute("data-section", section.id);
+        break;
+      }
+    };
 
     /** https://stackoverflow.com/a/69020644 */
-    const options: IntersectionObserverInit = {
-      rootMargin: "-50% 0% -50% 0%", // Makes the observer only look at the very center of the root for intersections
+    const observerOptions: IntersectionObserverInit = {
+      /** Makes the observer only look at the very center of the root for intersections */
+      rootMargin: "-50% 0% -50% 0%",
     };
-    const observer = new IntersectionObserver(callback, options);
 
-    for (const element of [profile, projects, contacts]) {
-      if (element === null) {
+    const observer = new IntersectionObserver(
+      observerCallback,
+      observerOptions
+    );
+
+    for (const section of sectionElementRecords) {
+      if (section.element === null) {
         console.warn(`An element to observe does not exist!`);
         continue;
       }
-      observer.observe(element);
+
+      observer.observe(section.element);
     }
 
     return () => {
@@ -72,48 +84,47 @@ export function Navbar() {
     ],
     "transition",
   ].join(" ");
-  const anchorProfile = [
-    anchor,
-    ...[
-      "bg-primary/50",
-      "hover:bg-primary",
-      "group-data-[section=who-am-i]/scrollspy:bg-primary",
-      "group-data-[section=talk-to-me]/scrollspy:bg-white/50",
-      "group-data-[section=talk-to-me]/scrollspy:hover:bg-white",
-    ],
-  ].join(" ");
-  const anchorProjects = [
-    anchor,
-    ...[
-      "bg-primary/50",
-      "hover:bg-primary",
-      "group-data-[section=what-i-do]/scrollspy:bg-primary",
-      "group-data-[section=talk-to-me]/scrollspy:bg-white/50",
-      "group-data-[section=talk-to-me]/scrollspy:hover:bg-white",
-    ],
-  ].join(" ");
-  const anchorContacts = [
-    anchor,
-    ...[
-      "bg-primary/50",
-      "hover:bg-primary",
-      // "group-data-[section=talk-to-me]/scrollspy:bg-primary", // Unnecessary as this condition has a definite color
-      "group-data-[section=talk-to-me]/scrollspy:bg-white",
-      "group-data-[section=talk-to-me]/scrollspy:hover:bg-white", // Should be redundant...
-    ],
-  ].join(" ");
+
+  const sectionClassesRecord: Record<SectionRecord["id"], string> = {
+    "who-am-i": [
+      anchor,
+      ...[
+        "bg-primary/50",
+        "hover:bg-primary",
+        "group-data-[section=who-am-i]/scrollspy:bg-primary",
+        "group-data-[section=talk-to-me]/scrollspy:bg-white/50",
+        "group-data-[section=talk-to-me]/scrollspy:hover:bg-white",
+      ],
+    ].join(" "),
+    "what-i-do": [
+      anchor,
+      ...[
+        "bg-primary/50",
+        "hover:bg-primary",
+        "group-data-[section=what-i-do]/scrollspy:bg-primary",
+        "group-data-[section=talk-to-me]/scrollspy:bg-white/50",
+        "group-data-[section=talk-to-me]/scrollspy:hover:bg-white",
+      ],
+    ].join(" "),
+    "talk-to-me": [
+      anchor,
+      ...[
+        "bg-primary/50",
+        "hover:bg-primary",
+        // "group-data-[section=talk-to-me]/scrollspy:bg-primary", // Unnecessary as this condition has a definite color
+        "group-data-[section=talk-to-me]/scrollspy:bg-white",
+        "group-data-[section=talk-to-me]/scrollspy:hover:bg-white", // Should be redundant...
+      ],
+    ].join(" "),
+  };
 
   return (
     <nav className={`z-10 ${navbarPos} flex md:grid gap-4 p-4`}>
-      <a href="#who-am-i" className={`${anchorProfile}`}>
-        <span className="sr-only">Jump to profile</span>
-      </a>
-      <a href="#what-i-do" className={`${anchorProjects}`}>
-        <span className="sr-only">Jump to projects</span>
-      </a>
-      <a href="#talk-to-me" className={`${anchorContacts}`}>
-        <span className="sr-only">Jump to contacts</span>
-      </a>
+      {SECTION_RECORDS.map((section) => (
+        <a href={`#${section.id}`} className={sectionClassesRecord[section.id]}>
+          <span className="sr-only">{section.hint}</span>
+        </a>
+      ))}
     </nav>
   );
 }
