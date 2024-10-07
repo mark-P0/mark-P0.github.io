31815f6cf7f61944aab8f08184671cc3fbeea513
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-10-07 20:10:20 +0800

    refactor(navbar): move section observer to own file

--- src/components/react/Navbar.tsx
+++ src/components/react/Navbar.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource react */
 
-import { useEffect, useState } from "react";
 import { SECTION_RECORDS, type SectionRecord } from "../../section-records.ts";
 import {
   Tooltip,
@@ -9,74 +8,7 @@ import {
   TooltipTrigger,
 } from "./shadcn/tooltip.tsx";
 import { cn } from "./shadcn/utils.ts";
-
-function useSectionObserver() {
-  const [visibleSectionId, setVisibleSectionId] = useState<
-    SectionRecord["id"] | null
-  >(null);
-
-  useEffect(() => {
-    const sectionElementRecords = SECTION_RECORDS.map(({ id }) => ({
-      id,
-      element: document.getElementById(id),
-      isVisible: false,
-    }));
-
-    const observerCallback: IntersectionObserverCallback = (entries) => {
-      /** On every callback run, assume none of the sections are visible */
-      for (const section of sectionElementRecords) {
-        section.isVisible = false;
-      }
-
-      for (const entry of entries) {
-        const { target, isIntersecting } = entry;
-        if (!isIntersecting) continue;
-
-        for (const section of sectionElementRecords) {
-          if (target !== section.element) continue;
-
-          section.isVisible = true;
-          break;
-        }
-      }
-
-      for (const section of sectionElementRecords) {
-        if (!section.isVisible) continue;
-
-        setVisibleSectionId(section.id);
-        break;
-      }
-    };
-
-    /** https://stackoverflow.com/a/69020644 */
-    const observerOptions: IntersectionObserverInit = {
-      /** Makes the observer only look at the very center of the root for intersections */
-      rootMargin: "-50% 0% -50% 0%",
-    };
-
-    const observer = new IntersectionObserver(
-      observerCallback,
-      observerOptions
-    );
-
-    for (const section of sectionElementRecords) {
-      if (section.element === null) {
-        console.warn(`An element to observe does not exist!`);
-        continue;
-      }
-
-      observer.observe(section.element);
-    }
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-  return {
-    visibleSectionId,
-  };
-}
+import { useSectionObserver } from "./useSectionObserver.tsx";
 
 function SectionLink(props: {
   section: SectionRecord;
